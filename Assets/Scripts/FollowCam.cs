4f7b4a7ea01eb9003bce83116f872097fa50241f
Austin Noska <noska.austin@gmail.com>
2020-10-17 23:29:27 -0400

    Create FollowCam script

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public static GameObject POI;
+
+    [Header("SetDynamically")]
+    public float camZ;
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null)
+            return;
+
+        Vector3 destination = POI.transform.position;
+        destination.z = camZ;
+        transform.position = destination;
+    }
+}
