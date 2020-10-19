ef1014d539403101972f94b08f2f1ad44138eb90
Austin Noska <noska.austin@gmail.com>
2020-10-19 17:20:28 -0400

    Create MissionDemolition script

---
+++ Assets/Scripts/MissionDemolition.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionDemolition : MonoBehaviour
+{
+    private static MissionDemolition S;
+
+    [Header("Set in Inspector")]
+    public Text uitLevel;
+    public Text uitShots;
+    public Text uitButton;
+
+    public Vector3 castlePos;
+    public GameObject[] castles;
+
+    [Header("Set Dynamically")]
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject castle;
+    public GameMode mode = GameMode.idle;
+    public string showing = "Show Slingshot";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        S = this;
+
+        level = 0;
+        levelMax = castles.Length;
+        StartLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGui();
+
+        if (mode == GameMode.playing && Goal.goalMet)
+        {
+            mode = GameMode.levelEnd;
+            SwitchView("Show Both");
+            Invoke("NextLevel", 2f);
+        }
+    }
+
+    public static void ShotsFired()
+    {
+        S.shotsTaken++;
+    }
+
+    public void SwitchView(string eView = "")
+    {
+        if (eView == "")
+            eView = uitButton.text;
+
+        showing = eView;
+        switch (showing)
+        {
+            case "Show Slingshot":
+                FollowCam.POI = null;
+                uitButton.text = "Show Castle";
+                break;
+
+            case "Show Castle":
+                FollowCam.POI = S.castle;
+                uitButton.text = "Show Both";
+                break;
+
+            case "Show Both":
+                FollowCam.POI = GameObject.Find("ViewBoth");
+                uitButton.text = "Show Slingshot";
+                break;
+        }
+    }
+
+    void StartLevel()
+    {
+        // Destroy old castles
+        if (castle != null)
+            Destroy(castle);
+
+        // Destroy old projectiles
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Projectile");
+        foreach (GameObject pTemp in gos)
+            Destroy(pTemp);
+
+        // Instantiate new castle
+        castle = Instantiate<GameObject>(castles[level]);
+        castle.transform.position = castlePos;
+        shotsTaken = 0;
+
+        // Reset camera
+        SwitchView("Show Both");
+        ProjectileLine.S.Clear();
+
+        // Reset goal
+        Goal.goalMet = false;
+
+        UpdateGui();
+
+        mode = GameMode.playing;
+    }
+
+    void NextLevel()
+    {
+        level++;
+        if (level == levelMax)
+            level = 0;
+
+        StartLevel();
+    }
+
+    void UpdateGui()
+    {
+        uitLevel.text = "Level: " + (level + 1) + " of " + levelMax;
+        uitShots.text = "Shots Taken: " + shotsTaken;
+    }
+}
+
+public enum GameMode
+{
+    idle,
+    playing,
+    levelEnd
+}
